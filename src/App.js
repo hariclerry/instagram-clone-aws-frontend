4dbaaa239481fce16d5c8c687d6b4fc073c5952f
Harriet Ayugi <clerryha@gmail.com>
2020-08-10 23:27:29 +0300

    add posts component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import Header from "./components/header/header";
+import Post from "./components/posts/posts";
+import postImage from "./assets/martin-bee.jpg";
+import postImage2 from "./assets/dylan-sauerwein-unsplash.jpg";
+import userAvatar from "./assets/harriet.jpeg";
+import userAvatar2 from "./assets/martin-bee.jpg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <section className="App-main">
+        <Post
+          nickname="Harriet"
+          avatar={userAvatar}
+          caption="Amazing post!"
+          image={postImage}
+        />
+        <Post
+          nickname="RockStar"
+          avatar={userAvatar2}
+          caption="Holding a mic"
+          image={postImage2}
+        />
+
+        {/* more posts */}
+      </section>
     </div>
   );
 }
